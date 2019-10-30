8ef3e1ed50178c265b30b831e61c01b50f5674f3
Passion Dev <az.fullstack2258@gmail.com>
2019-10-30 03:33:20 -0700

    misc: add moment, fix lint errors

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -9,6 +9,12 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+
+    <q-footer elevated>
+      <q-toolbar>
+        <q-toolbar-title>Footer</q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
   </q-layout>
 </template>
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,11 @@
 <template>
-  <q-page class="flex flex-center"></q-page>
+  <q-page class="flex flex-center">
+    <div class="column">
+      <div class="col-3">Two Fish Resort</div>
+      <div class="col">Date</div>
+      <div class="col-4">Content</div>
+    </div>
+  </q-page>
 </template>
 
 <script>
