f62ac985a8eab31fb62c2f2ba3d2040c8bc209e5
Passion Dev <az.fullstack2258@gmail.com>
2019-10-30 21:07:14 -0700

    feat: add summary component

--- src/pages/enquiry/Activities.vue
+++ src/pages/enquiry/Activities.vue
@@ -1,8 +1,16 @@
 <template>
-  <p>What will you do?</p>
+  <div class="q-pa-md">
+    <Report />
+    <p>What will you do?</p>
+  </div>
 </template>
 <script>
+import Report from "./Report";
+
 export default {
-  name: "Activities"
+  name: "Activities",
+  components: {
+    Report
+  }
 };
 </script>
---
+++ src/pages/enquiry/Report.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>Dive resort - Bunaken</p>
+    <p class="text-h2">Two fish</p>
+    <q-separator />
+    <p>{{ `On ${date} `}}</p>
+    <p>{{ guestCount }}</p>
+    <q-separator />
+    <p class="text-h4">Total (USD)</p>
+  </div>
+</template>
+<script>
+import { mapState } from "vuex";
+import Pluralize from "pluralize";
+
+export default {
+  name: "Report",
+  computed: {
+    ...mapState("resort", ["date", "diverCount", "nonDiverCount"]),
+    guestCount() {
+      return Pluralize("guest", this.diverCount + this.nonDiverCount, true);
+    }
+  }
+};
+</script>
