6e528be0f528765a70dc006b2581405b44ce158e
Passion Dev <az.fullstack2258@gmail.com>
2019-10-31 02:13:43 -0700

    feat: validate selected diver/non-diver packages

--- src/pages/enquiry/Index.vue
+++ src/pages/enquiry/Index.vue
@@ -4,7 +4,14 @@
       <q-step :name="1" title="Activities" icon="insert_emoticon" :done="done1">
         <Activities />
         <q-stepper-navigation>
-          <q-btn @click="() => { done1 = true; step = 2 }" color="primary" label="Continue" />
+          <q-btn
+            @click="() => { done1 = true; step = 2 }"
+            color="primary"
+            label="Continue"
+            :disable="getDivers === 0"
+          />
+          <p v-if="validateDiverPackages">{{ validateDiverPackages }}</p>
+          <p v-if="validateNonDiverPackages">{{ validateNonDiverPackages }}</p>
         </q-stepper-navigation>
       </q-step>
 
@@ -27,6 +34,7 @@
   </div>
 </template>
 <script>
+import { mapState, mapGetters } from "vuex";
 import Activities from "./Activities";
 import Details from "./Details";
 import Review from "./Review";
@@ -46,7 +54,29 @@ export default {
       done3: false
     };
   },
-
+  computed: {
+    ...mapState("resort", ["diverCount", "nonDiverCount"]),
+    ...mapGetters("enquiry", ["getDivers", "getNonDivers"]),
+    validateDiverPackages() {
+      if (this.getDivers === 0) {
+        return "At least one diver package should be selected";
+      }
+      if (this.diverCount < this.getDivers) {
+        return "More diver packages are selected than guests";
+      } else if (this.diverCount > this.getDivers) {
+        return "Less diver packages are selected than guests";
+      }
+      return "";
+    },
+    validateNonDiverPackages() {
+      if (this.nonDiverCount < this.getNonDivers) {
+        return "More non-diver packages are selected than guests";
+      } else if (this.nonDiverCount > this.getNonDivers) {
+        return "Less non-diver packages are selected than guests";
+      }
+      return "";
+    }
+  },
   methods: {}
 };
 </script>
